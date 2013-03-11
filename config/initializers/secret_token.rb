0fb2465cbfd5243be63994de395f9740f29a3683
Louis Rose <louis.rose@york.ac.uk>
2013-03-11 09:48:27 +0000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coco::Application.config.secret_token = '172e2ca291b45ef60a3302dcdadf7788b7dae66f4a71991c0884cec2921b2f33a7ac155e6dd770df29c9280b91cc39699a9beb8a33557965de48cbd5e9bbff8d'
