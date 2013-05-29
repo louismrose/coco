3829f9cf3294ad1d7aa29c478d1f503f1bc6c183
Louis Rose <louis.rose@york.ac.uk>
2013-05-29 17:45:55 +0100

    Perform string reversal in Java.

--- app/workers/instance_runner.rb
+++ app/workers/instance_runner.rb
@@ -1,8 +1,14 @@
+require "transformers/reverser.jar"
+
 class InstanceRunner
   @queue = :instances
   def self.perform(instance_id)
     sleep(10)
     instance = Instance.find(instance_id)
-    instance.update_attribute(:input_model, instance.input_model.reverse)
+    instance.update_attribute(:input_model, reverse(instance.input_model))
+  end
+  
+  def self.reverse(subject)
+    Java::String::Reverser.new(subject).run
   end
 end
