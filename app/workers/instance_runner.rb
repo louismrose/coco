53601406973f840e32dcc450384781362b32a053
Louis Rose <louis.rose@york.ac.uk>
2013-06-18 20:11:30 +0100

    Fix typo.

--- app/workers/instance_runner.rb
+++ app/workers/instance_runner.rb
@@ -10,13 +10,13 @@ class InstanceRunner
   
   def self.perform(instance_id)
     puts "Rails logger is: " + Rails.logger.inspect
-    puts "Logger is: " + ActiveSupport::LogSubscriber.logger
+    puts "Logger is: " + ActiveSupport::LogSubscriber.logger.inspect
      
     instance = Instance.find(instance_id)
     InstanceRunner.new(instance).transform
     
     puts "Rails logger is: " + Rails.logger.inspect
-    puts "Logger is: " + ActiveSupport::LogSubscriber.logger
+    puts "Logger is: " + ActiveSupport::LogSubscriber.logger.inspect
     
     instance.save
   end
