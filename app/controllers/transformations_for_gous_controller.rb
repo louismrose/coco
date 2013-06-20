b139afbebddceaed1e61620150b8a7d4d13563ab
Louis Rose <louis.rose@york.ac.uk>
2013-06-20 11:18:52 +0100

    Fix typo.

--- app/controllers/transformations_for_gous_controller.rb
+++ app/controllers/transformations_for_gous_controller.rb
@@ -7,7 +7,7 @@ class TransformationsForGousController < ApplicationController
     render json: {
       grammar: File.read(File.join(Rails.root, @transformation.grammar_file)),
       evaluation_url: new_transformation_instance_for_gous_url(@transformation, format: :json),
-      batch_evaluation_url: new_transformation_instances_for_gous(@transformation, format: :json)
+      batch_evaluation_url: new_transformation_instances_for_gous_url(@transformation, format: :json)
     }
   end
 end
